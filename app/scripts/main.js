20d155a912e1c49435fb4353fb8bbf692f6271cb
eighthnote <don.helm@eighthnotemedia.com>
2015-04-11 22:50:58 +0000

    sets up scrollmagic for nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,15 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+    if (!Modernizr.touch) {
+        // init controller
+        var controller = new ScrollMagic.Controller();
+        
+        // create a scene
+        new ScrollMagic.Scene({
+                offset: 200      // start this scene after scrolling for 50px
+            })
+            .setPin("nav")
+            .setClassToggle("body", "pinned")// pins the element for the the scene's duration
+            .addTo(controller); // assign the scene to the controller
+    }
+});
